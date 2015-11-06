db30427bca701caa06c6d2dba1b5ed6539bc1aee
Cameron <cameronpdixon@gmail.com>
2015-11-06 10:49:20 -0500

    Added Box2d debug renderer

--- core/src/com/cam/camsgame/Screens/PlayScreen.java
+++ core/src/com/cam/camsgame/Screens/PlayScreen.java
@@ -4,12 +4,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.cam.camsgame.CamsGame;
 import com.cam.camsgame.Scenes.Hud;
+
+import java.awt.Rectangle;
 
 /**
  * Created by Cameron on 2015-11-04.
@@ -18,11 +29,17 @@ public class PlayScreen implements Screen {
     private CamsGame game;
     private OrthographicCamera gamecam;
     private FitViewport gameport; //https://www.youtube.com/watch?v=D7u5B2Oh9r0 <-- maintaining aspect ratios
+
+    //Hud
     private Hud hud;
 
+    //Tiled
     private TmxMapLoader mapLoader; //https://www.youtube.com/watch?v=P8jgD-V5jG8 <-- how the map is loaded
     private TiledMap tlMap;
     private OrthogonalTiledMapRenderer tlRender;
+
+    private World world;
+    private Box2DDebugRenderer b2dr;
 
     public PlayScreen(CamsGame game){
         this.game = game;
@@ -35,11 +52,36 @@ public class PlayScreen implements Screen {
         hud = new Hud(game.batch);
 
         mapLoader = new TmxMapLoader();
+
         //Load tiled map
         tlMap = mapLoader.load("Maps/Map.tmx");
         tlRender = new OrthogonalTiledMapRenderer(tlMap);
+
         //Set the gamecams position to half of the width and height of the map (the center of the map)
         gamecam.position.set(gameport.getWorldWidth()/2, gameport.getWorldHeight()/2, 0);
+
+        //no gravity in the vector 2
+        world = new World(new Vector2(0,0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bDef = new BodyDef();
+        PolygonShape psShape = new PolygonShape();
+        FixtureDef fDef = new FixtureDef();
+        Body bBody;
+
+        for(MapObject object :  tlMap.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)){ //https://www.youtube.com/watch?v=AmLDslUdepo
+
+            com.badlogic.gdx.math.Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            //Static means it's not affected by forces unless its programmed to be
+            bDef.type = BodyDef.BodyType.StaticBody;
+            bDef.position.set(rect.getX() + rect.getWidth()/2, rect.getY() + rect.getHeight()/2);
+
+            bBody = world.createBody(bDef);
+
+            psShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fDef.shape = psShape;
+            bBody.createFixture(fDef);
+        }
     }
 
     @Override
@@ -57,10 +99,12 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         //Calls update to instantly update to the map
         update();
-        Gdx.gl.glClearColor(1,0,1,0);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         // renders the map
         tlRender.render();
+        //Renders our debug lines for box2d
+        b2dr.render(world, gamecam.combined);
         //Gets whats shown by our huds camera
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         //Draws the hud to the screen
